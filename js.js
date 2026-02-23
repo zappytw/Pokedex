65ce61d0c5e00017ba293bd72d68edf242f74fdd
Joel <zappythehawk@gmail.com>
2026-02-23 08:59:11 -0500

    feat: :sparkles: Sistema de busqueda

--- js.js
+++ js.js
@@ -8,13 +8,51 @@ async function fetchAllPokes() {
     return allPokes
 }
 
-async function filterPokes() {
-    let filteredPokes = await fetchAllPokes
+async function filterPokes(name) {
+    let allPokes = await fetchAllPokes()
+    let filteredPokes = allPokes.results.filter(poke=>poke.name.includes(name))
+    showPokes(filteredPokes)
     return filteredPokes
 }
 
-async function showPokes() {
+async function showPokes(filteredPokes) {
+    Container.innerHTML=""
+    for(const pokemon of filteredPokes){
+        response = await fetch(pokemon.url)
+        pokeData = await response.json()
+
+        let pokeDiv = document.createElement("div")
+        let pokeImg = document.createElement("img")
+        let pokeName = document.createElement("p")
+        let pokeId = document.createElement("p")
+        let pokeType1 = document.createElement("p")
+
+        pokeDiv.classList.add("pokeDiv")
+        pokeImg.classList.add("pokeImg")
+        pokeName.classList.add("pokeName")
+        pokeId.classList.add("pokeId")
+        pokeType1.classList.add("pokeType")
+
+        pokeImg.src=pokeData.sprites.front_default
+        pokeName.textContent= pokeData.name
+        pokeId.textContent="#" + pokeData.id
+        pokeType1.textContent= pokeData.types[0].type.name
+        pokeDiv.append(pokeImg, pokeName, pokeId, pokeType1)
+
+        if(pokeData.types[1]!==undefined){
+            let pokeType2 = document.createElement("p")
+            pokeType2.textContent = pokeData.types[1].type.name
+            pokeType2.classList.add("pokeType")
+            pokeDiv.append(pokeType2)
+        }
+
+        Container.append(pokeDiv)
+    }
     
 }
 
-console.log(filterPokes())
+Form.addEventListener("submit", async (event)=>{
+    event.preventDefault();
+    let searchQuery = Input.value
+    filterPokes(searchQuery)
+})
